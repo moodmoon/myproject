aa77b453b652575e68aaa334f7f80cc762dc6cdc
Pearmai <pearmai.wann@kmutt.ac.th>
2023-03-20 14:37:15 +0700

    commit message

---
+++ Myproject.java
@@ -0,0 +1,12 @@
+package myproject;
+
+public class Myproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Pearmai");
+    }
+    
+}
