f78733a342e0787f72bf17b244bc001745cffa73
Pearmai <pearmai.wann@kmutt.ac.th>
2023-03-20 14:47:34 +0700

    modified file

--- Myproject.java
+++ Myproject.java
@@ -6,7 +6,7 @@ public class Myproject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Pearmai");
+        System.out.println("Hello Worlds!");
     }
     
 }
